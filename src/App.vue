864dc70e67bebf40385c236dcccddc81943cbb53
xuanweiH <525405891@qq.com>
2019-05-07 15:58:38 +0800

    项目结构基本搭建 首页头部和导航栏样式完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MHeader></MHeader>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import MHeader from './components/m-header/m-header'
+  import Tab from './components/tab/tab'
+  export default {
+    components: {
+        MHeader,
+        Tab
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/variable";
+  
+  #app 
+    color: $color-theme
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/router'
 
+import fastclick from 'fastclick'
+import './common/stylus/index.styl'
+
+ fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
